ea4113e3a67af3495e62d82b3189a7179f568f52
Gunnar Oledal <gunnar.oledal@gmail.com>
2021-01-19 18:21:14 +0100

    Adding some script files

---
+++ backend/prepare-spa-build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Building and copying SPA. This scripts outputs the built app into the caddy folder."
+echo "This is so that the caddy container can copy the app into it's filesystem when building the container."
+echo ""
+
+# echo "reading .env file and add the stuff to bash environment"
+# if [ -f .env ]
+# then
+#   export $(cat .env | sed 's/#.*//g' | xargs)
+# fi
+
+# printenv > .env.dev
+
+echo "copying .env to app folder so variables get injected into the built app"
+cp .env ../app/.env
+
+echo "running npm install"
+npm --prefix ../app install
+echo "running npm build"
+npm --prefix ../app run build
+
+echo "removing previous build"
+rm -r ../caddy/spa/
+
+echo "copy new build into spa folder ../caddy/spa/"
+cp -r ../app/dist/spa/ ./caddy/spa/
