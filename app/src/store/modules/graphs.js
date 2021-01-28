b615c34d3841fa929b86987f4bdecbfb3b9d139c
Martin Törnros <martin.tornros@ri.se>
2021-01-28 16:05:56 +0100

    Highcharts

---
+++ app/src/store/modules/graphs.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default {
+  namespaced: true,
+  state: {
+    graphs: []
+  },
+  getters: {},
+  mutations: {
+  },
+  actions: {
+  }
+}
