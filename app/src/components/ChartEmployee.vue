ca4da662852fc8f6b62edeaf506399d5ff318f47
Martin Törnros <martin.tornros@ri.se>
2021-01-29 11:28:04 +0100

    Chart styling

--- app/src/components/ChartEmployee.vue
+++ app/src/components/ChartEmployee.vue
@@ -20,12 +20,19 @@ export default {
       required: true
     }
   },
+  data: function () {return {
+    keys: ['from', 'to']
+  };},
   mounted: function () {
+    this.styleChart();
     this.initChart();
   },
   methods: {
     initChart: function () {
+      var self = this;
       const g = this.graph;
+      const dataMap = g.edges.map(e => function (){return{from: e.source, to:e.target};}());
+      console.log(dataMap);
       Highcharts.chart('container', {
         chart: {
           type: 'networkgraph',
@@ -39,7 +46,7 @@ export default {
         },
         plotOptions: {
           networkgraph: {
-            keys: ['from', 'to'],
+            keys: self.keys,
             layoutAlgorithm: {
               enableSimulation: true,
               friction: -0.9
@@ -51,10 +58,79 @@ export default {
             enabled: true,
             linkFormat: ''
           },
-          id: 'lang-tree',
-          data: g.edges.map(e => [e.source, e.target])
+          link: {
+            color: 'black'
+          },
+          id: 'chart-employee',
+          data: dataMap
         }]
       });
+    },
+    styleChart: function () {
+      const self = this;
+      const g = this.graph;
+      Highcharts.addEvent(
+        Highcharts.Series,
+        'afterSetOptions',
+        function (e) {
+          
+          var nodes = {};
+
+          if (
+            this instanceof Highcharts.seriesTypes.networkgraph &&
+            e.options.id === 'chart-employee'
+          ) {
+            e.options.data.forEach(function (link) {
+
+              link.width = self.getWeight(link.from,link.to) < 0.4 ? 1 : 3;
+              console.log(link.width);
+              if (link.from === g['self-id']) {
+                nodes[g['self-id']] = {
+                  id: g['self-id'],
+                  marker: {
+                    radius: 10
+                  }
+                };
+              } else {
+
+                self.keys.forEach((key) => {
+                  nodes[link[key]] = {
+                    id: link[key],
+                    color: self.isCoAuthor(link[key]) ? '#66ca66' : '#fa6666',
+                    marker: {
+                      radius: self.isCoAuthor(link[key]) ? 8 : 5,
+                    }
+                    
+                  };
+                });
+
+              }
+
+
+            });
+
+            
+
+            e.options.nodes = Object.keys(nodes).map(function (id) {
+              return nodes[id];
+            });
+          }
+        }
+      );
+    },
+    isCoAuthor: function (id) {
+      var node = this.graph.nodes.find(n => n.id === id);
+      if (node){
+        return node['co-author'];
+      }
+      return false;
+    },
+    getWeight: function (from,to) {
+      var edge = this.graph.edges.find(e => e.source === from && e.target === to);
+      if (edge){
+        return edge['weight'];
+      }
+      return 0.0;
     }
   }
 };
